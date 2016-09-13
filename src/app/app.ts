33e4d98b5ee8124c8f725e24227cc96741f9d6ca
Gerard Sans <gerard.sans@gmail.com>
2016-09-13 16:30:18 +0100

    Removed unused files

--- src/app/app.ts
+++
@@ -1,8 +0,0 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-
-@Component({
-  selector: 'app',
-  styleUrls: ['./app/app.css'],
-  encapsulation: ViewEncapsulation.None
-})
-export class App { }
