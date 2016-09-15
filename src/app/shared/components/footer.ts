1c3cb0915faa521db6021ed80b3c8afb56d84d39
Gerard Sans <gerard.sans@gmail.com>
2016-09-15 17:47:37 +0100

    Basic contact details footer

--- src/app/shared/components/footer.ts
+++ src/app/shared/components/footer.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'myfooter',
-  template: `<div class="section">Footer</div>`
+  template: `
+    <div class="myfooter centerBlock">
+      <a href="https://twitter.com/ngcruise" target="_blank">@ngcruise</a>
+      | <a href="mailto:howdy@ngcruise.com">howdy@ngcruise.com</a>
+    </div>
+  `
 })
 export class Footer {
   constructor() { }
