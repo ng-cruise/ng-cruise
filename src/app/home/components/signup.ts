48ee1b89406e4e423b750c1589a0f95d65f9a8a7
Gerard Sans <gerard.sans@gmail.com>
2016-09-15 21:20:49 +0100

    Signup with Mailchimp

--- src/app/home/components/signup.ts
+++ src/app/home/components/signup.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'signup',
   template: `
     <div class="section" id="signup">Signup</div>
+    <div class="centerBlock clearfix">
+      <button class="main-btn grow" onclick="document.location='http://eepurl.com/cfYyWP';return false;">Signup</button>
+    </div>
     <section-footer left="assets/img/24_small.png" right="assets/img/20_small.png" bottomClass="section-footer__sea" waves="true"></section-footer>
   `
 })
