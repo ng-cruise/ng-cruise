72f5577e2e0d9f0f0694fdc589e0e9317547dd26
Gerard Sans <gerard.sans@gmail.com>
2016-09-20 20:31:27 +0100

    New assets for header

--- src/app/home/components/welcome.ts
+++ src/app/home/components/welcome.ts
@@ -4,17 +4,17 @@ import { Component } from '@angular/core';
   selector: 'welcome',
   template: `
     <div id="welcome" class="welcome-container centerBlock">
-      <div class="welcome hidden-xs"></div>
+      <div class="welcome"></div>
       <div class="welcome-title">Angular.JS Cruise</div>
       <div class="welcome-sub">
         Sub Headline Lorem Ipsum Dolor Sit<br/>
-        <a href="https://www.ng-conf.org/#/conduct" title="This code of conduct was adopted from ng-conf's code of conduct" target="_blank">Code of Conduct</a>
+        <a href="#/conduct" title="This code of conduct was adopted from ng-conf's code of conduct">Code of Conduct</a>
       </div>
       <div class="centerBlock">
-        <button class="main-btn grow" onclick="document.location='/#/home#tickets';return false;">Purchase</button> 
+        <button class="main-btn grow" onclick="window.open('http://eepurl.com/cfYyWP','_blank');return false;">Signup</button> 
       </div>
       <div class="centerBlock">
-        <button class="main-btn grow" onclick="document.location='/#/home#sponsors';return false;">Sponsor</button>
+        <button class="main-btn grow" onclick="window.open('https://goo.gl/forms/bbdH7XOcRtt6ciz23','_blank');return false;">Sponsor</button>
       </div>
     </div>
   `
