5989cde29100fb5db17c144b7afbe2d5d5356125
Gerard Sans <gerard.sans@gmail.com>
2016-09-23 21:26:23 +0100

    Fixed top navigation links

--- src/app/shared/components/header.ts
+++ src/app/shared/components/header.ts
@@ -20,20 +20,20 @@ import { Component } from '@angular/core';
                 <div class="centerBlock" routerLinkActive="active"><a [routerLink]="['home']" fragment="speakers">Speakers</a></div>
               </div>
               <div class="col-xs-2 menu">
-                <div class="centerBlock"><a [routerLink]="['home']" fragment="schedule">Schedule</a></div>
+                <div class="centerBlock"><a [routerLink]="['home']" fragment="tickets">Tickets</a></div>
               </div>
               <div class="col-xs-2">
                 <div class="centerBlock">
                   <div class="logo" [routerLink]="['home']" fragment="top">
-                    <div class="logo-icon centerBlock"><img src="assets/img/logo.png"></div>
+                    <div class="logo-icon centerBlock"><img src="assets/img/NG-Cruise-Logo-Normal.png"></div>
                   </div>
                 </div>
               </div>
               <div class="col-xs-2 menu">
-                <div class="centerBlock"><a [routerLink]="['home']" fragment="sponsors">Sponsors</a></div>
+                <div class="centerBlock"><a href="#" onclick="window.open('https://goo.gl/forms/bbdH7XOcRtt6ciz23','_blank');return false;" target="_blank">Sponsors</a></div>
               </div>
               <div class="col-xs-2 menu">
-                <div class="centerBlock"><a [routerLink]="['cruise']">Cruise</a></div>
+                <div class="centerBlock"><a [routerLink]="['home']" fragment="location">Cruise</a></div>
               </div>
             </div>
             <div class="visible-xs">
