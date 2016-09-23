1b1200cc8d99d5fca079d462299d8101ee1db224
Gerard Sans <gerard.sans@gmail.com>
2016-09-23 21:26:01 +0100

    Changes to tickets section

--- src/app/home/components/tickets.ts
+++ src/app/home/components/tickets.ts
@@ -5,9 +5,12 @@ import { Component } from '@angular/core';
   template: `
     <div class="section" id="tickets">Tickets</div>
     <div class="headline__body">
-      Please review our <a href="#/conduct">Code of Conduct</a> before doing your purchase. 
+      Please review our <a href="#/conduct">Code of Conduct</a> before your purchase. 
     </div>
-    <tito-widget event="ultimateconf/2013"></tito-widget>
+    <div class="col-s-12">
+      <h4 class="centerBlock">Tickets opens 10/15/2016.</h4>
+    </div>
+    <!-- <tito-widget event="ultimateconf/2013"></tito-widget> -->
     <section-footer left="assets/img/beach-umbrella.png" right="assets/img/35_small.png" bottomClass="section-footer__beach_lg hole"></section-footer>
   `
 })
