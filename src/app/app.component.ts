22299c700a555f96fc7aaaa34dcd821f9d951fce
Gerard Sans <gerard.sans@gmail.com>
2017-02-02 17:59:58 +0000

    Fixed scrolling issue.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@
  * Angular 2 decorators and services
  */
 import { Component, ViewEncapsulation } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { AppState } from './app.service';
 
@@ -23,4 +24,26 @@ import { AppState } from './app.service';
     <myfooter></myfooter>
   `
 })
-export class App { }
+export class App {
+  constructor(router: Router) {
+    var width = window.innerWidth
+      || document.documentElement.clientWidth
+      || document.body.clientWidth;
+    var offset = (width<768)? 260 : 160;
+
+    //fix(#6595) https://github.com/angular/angular/issues/6595
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url);
+        if (tree.fragment) {
+          // you can use DomAdapter
+          const element:any = document.querySelector("#" + tree.fragment);
+          if (element) { 
+            element.scrollIntoView(element); 
+            document.body.scrollTop -= offset;
+          }
+        }
+      }
+    });
+  }
+ }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,27 +7,4 @@ import { Router, NavigationEnd } from '@angular/router';
   templateUrl: './home.template.html',
   encapsulation: ViewEncapsulation.None
 })
-export class Home {
-  constructor(router: Router) {
-    console.log('Home');
-    var width = window.innerWidth
-      || document.documentElement.clientWidth
-      || document.body.clientWidth;
-    var offset = (width<768)? 260 : 160;
-
-    //fix(#6595) https://github.com/angular/angular/issues/6595
-    router.events.subscribe(s => {
-      if (s instanceof NavigationEnd) {
-        const tree = router.parseUrl(router.url);
-        if (tree.fragment) {
-          // you can use DomAdapter
-          const element:any = document.querySelector("#" + tree.fragment);
-          if (element) { 
-            element.scrollIntoView(element); 
-            document.body.scrollTop -= offset;
-          }
-        }
-      }
-    });
-  }
-}
+export class Home { }
